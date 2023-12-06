3d9e86ee32829ceeba6a11ed3b8b267f60a6363f
claire256 <clairenabwami3@gmail.com>
2023-12-06 22:52:26 +0300

    first commit

---
+++ Guess/script.js
@@ -0,0 +1,25 @@
+// Random number between 1 and 100
+
+const randomNumber = 1+ Math.floor(100*Math.random())
+
+let tries = 0
+
+function checkGuessedNumber(){
+  
+    const userInput = document.getElementById("inputGuess")
+    const inputGuess = parseInt(userInput.value);
+    
+    tries++
+
+    if (isNaN(inputGuess) || inputGuess < 1 || inputGuess > 100) {
+        document.getElementById("output").textContent = "Please enter a valid number between 1 and 100.";
+      } else if (inputGuess === randomNumber) {
+        document.getElementById("output").textContent = `Congratulations! You guessed the correct number ${randomNumber} in ${tries} attempts.`;
+      } else if (inputGuess < randomNumber) {
+        document.getElementById("output").textContent = "Too low! Try again.";
+      } else {
+        document.getElementById("output").textContent = "Too high! Try again.";
+      }
+      
+      userInput.value = "";
+}
